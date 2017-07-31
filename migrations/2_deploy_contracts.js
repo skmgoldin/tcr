55ce9cbddb8dcbbb29471b98ebdb5bfd3d9a3d23
Yorke Rhodes IV <yorke.rhodesiv@consensys.net>
2017-07-31 15:40:43 -0400

    Modularize migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,23 +1,37 @@
 var Registry = artifacts.require("./Registry.sol");
 var Token = artifacts.require("./HumanStandardToken.sol");
 var Voting = artifacts.require("./PLCRVoting.sol");
+
+const fs = require("fs");
+
 module.exports = function(deployer) {
-  deployer.deploy(Token,100000,'adtoken', "3", "ATK").then(function(){
-  	return Token.deployed();
-  }).then(function(_token){
-  	token = _token;
-  })
-  .then(function(){
-  	return deployer.deploy(Voting,0);
-  })
-  .then(function(){
-  	return Voting.deployed();
-  })
-  .then(function(voting){
-   return deployer.deploy(Registry, token.address,voting.address, 50,50,100,100,100,100,50,50)
-  });
+    let adchainConfig = JSON.parse(fs.readFileSync('./conf/config.json'));
+    let tokenConfig = adchainConfig.TokenArguments;
+    let registryConfig = adchainConfig.RegistryDefaults;
+
+    // console.log("token", tokenConfig);
+    // console.log("registry", registryConfig);
+
+    deployer.deploy(
+        Token,
+        tokenConfig.totalSupply,
+        tokenConfig.name,
+        tokenConfig.decimalUnits,
+        tokenConfig.symbol
+    ).then(() => {
+        return deployer.deploy(
+            Registry,
+            Token.address,
+            registryConfig.minDeposit,
+            registryConfig.applyStageLength,
+            registryConfig.commitPeriodLength,
+            registryConfig.revealPeriodLength,
+            registryConfig.dispensationPct,
+            registryConfig.voteQuorum
+        );
+    });
 };
 // module.exports = function(deployer) {
-//   deployer.deploy(Token);
-//   deployer.deploy(Registry)
+//     deployer.deploy(Token);
+//     deployer.deploy(Registry)
 // };
