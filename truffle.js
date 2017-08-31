d7c7ca37d1b95e2da4dcfc4fcde02ba10a3e29ae
Isaac Kang <isaacjkang@gmail.com>
2017-08-31 18:09:16 -0400

    change rinkeby provider url

--- truffle.js
+++ truffle.js
@@ -19,7 +19,7 @@ module.exports = {
       network_id: '*', // Match any network id
     },
     rinkeby: {
-      provider: new HDWalletProvider(mnemonic, 'https://testrpc.adchain.com:443'),
+      provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io'),
       network_id: '*',
       gas: 4500000,
       gasPrice: 25000000000,
