0b850613f5c5deb3d084568fb54aa3f8e5693887
Isaac Kang <isaacjkang@gmail.com>
2017-09-06 14:04:09 -0400

    add addToWhiteList util func.

--- test/utils.js
+++ test/utils.js
@@ -4,6 +4,7 @@
 const HttpProvider = require('ethjs-provider-http');
 const EthRPC = require('ethjs-rpc');
 const abi = require('ethereumjs-abi');
+const fs = require('fs');
 
 const ethRPC = new EthRPC(new HttpProvider('http://localhost:8545'));
 
@@ -11,6 +12,9 @@ const Token = artifacts.require('./HumanStandardToken.sol');
 const PLCRVoting = artifacts.require('./PLCRVoting.sol');
 const Registry = artifacts.require('./Registry.sol');
 const Sale = artifacts.require('historical/Sale.sol');
+
+const adchainConfig = JSON.parse(fs.readFileSync('./conf/config.json'));
+const paramConfig = adchainConfig.RegistryDefaults;
 
 let registry;
 let token;
@@ -81,6 +85,34 @@ const utils = {
     await approvePLCRFor(accounts.slice(1));
 
     return [registry, token, applicant, challenger, voter];
+  },
+  addToWhitelist: async (domain, deposit, actor) => {
+    await utils.as(actor, registry.apply, domain, deposit);
+    await utils.increaseTime(paramConfig.applyStageLength + 1);
+    await utils.as(actor, registry.updateStatus, domain);
+  },
+  as: (actor, fn, ...args) => {
+    function detectSendObject(potentialSendObj) {
+      function hasOwnProperty(obj, prop) {
+        const proto = obj.constructor.prototype;
+        return (prop in obj) &&
+          (!(prop in proto) || proto[prop] !== obj[prop]);
+      }
+      if (typeof potentialSendObj !== 'object') { return undefined; }
+      if (
+        hasOwnProperty(potentialSendObj, 'from') ||
+        hasOwnProperty(potentialSendObj, 'to') ||
+        hasOwnProperty(potentialSendObj, 'gas') ||
+        hasOwnProperty(potentialSendObj, 'gasPrice') ||
+        hasOwnProperty(potentialSendObj, 'value')
+      ) {
+        throw new Error('It is unsafe to use "as" with custom send objects');
+      }
+      return undefined;
+    }
+    detectSendObject(args[args.length - 1]);
+    const sendObject = { from: actor };
+    return fn(...args, sendObject);
   },
 };
 
