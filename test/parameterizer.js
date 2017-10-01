b5044c71dbb04b46b1dc6d45e808308d48630f19
Isaac Kang <isaacjkang@gmail.com>
2017-10-01 16:17:56 -0400

    add test and util: get value from receipt logs

--- test/parameterizer.js
+++ test/parameterizer.js
@@ -10,9 +10,13 @@ const utils = require('./utils');
 const adchainConfig = JSON.parse(fs.readFileSync('./conf/config.json'));
 const paramConfig = adchainConfig.RegistryDefaults;
 
+const bigTen = number => new BN(number.toString(10), 10);
+const getReceiptValue = (receipt, arg) => receipt.logs[0].args[arg];
+
 contract('Parameterizer', (accounts) => {
   describe('Function: proposeReparameterization', () => {
-    const [proposer] = accounts;
+    const [proposer, secondProposer] = accounts;
+    const pMinDeposit = bigTen(paramConfig.pMinDeposit);
 
     it('should add a new reparameterization proposal', async () => {
       const parameterizer = await Parameterizer.deployed();
@@ -24,14 +28,14 @@ contract('Parameterizer', (accounts) => {
         proposer, parameterizer.proposeReparameterization, 'voteQuorum', '51',
       );
 
-      const propID = receipt.logs[0].args.propID;
+      const propID = getReceiptValue(receipt, 'propID');
       const paramProposal = await parameterizer.proposalMap.call(propID);
 
       assert.strictEqual(paramProposal[6].toString(10), '51', 'The reparameterization proposal ' +
         'was not created, or not created correctly.');
 
       const applicantFinalBalance = await token.balanceOf.call(proposer);
-      const expected = applicantStartingBalance.sub(new BN(paramConfig.pMinDeposit, 10));
+      const expected = applicantStartingBalance.sub(pMinDeposit);
       assert.strictEqual(applicantFinalBalance.toString(10), expected.toString(10),
         'tokens were not properly transferred from proposer');
     });
@@ -44,6 +48,19 @@ contract('Parameterizer', (accounts) => {
           proposer, parameterizer.proposeReparameterization, 'voteQuorum', '51',
         );
         assert(false, 'Performed NOOP reparameterization');
+      } catch (err) {
+        assert(utils.isEVMException(err), err.toString());
+      }
+    });
+
+    it('should not allow a reparameterization for a proposal that already exists', async () => {
+      const parameterizer = await Parameterizer.deployed();
+
+      try {
+        await utils.as(
+          secondProposer, parameterizer.proposeReparameterization, 'voteQuorum', '51',
+        );
+        assert(false, 'should not have been able to make duplicate proposal');
       } catch (err) {
         assert(utils.isEVMException(err), err.toString());
       }
