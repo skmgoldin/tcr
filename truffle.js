30cca2c14a59b9422c9961a5816bda156de24607
Mike Goldin <mike.goldin@consensys.net>
2018-01-14 14:31:38 -0500

    Add mainnet deploy scripts

--- truffle.js
+++ truffle.js
@@ -6,11 +6,17 @@ let mnemonic = '';
 
 if (fs.existsSync('secrets.json')) {
   secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
-  ({ mnemonic } = secrets.mnemonic);
+  ({ mnemonic } = secrets);
 }
 
 module.exports = {
   networks: {
+    mainnet: {
+      provider: new HDWalletProvider(mnemonic, 'https://mainnet.infura.io'),
+      network_id: '1',
+      gas: 4500000,
+      gasPrice: 10000000000,
+    },
     rinkeby: {
       provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io'),
       network_id: '*',
