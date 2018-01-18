f56b3fa41de91442dc4637bcb8f1fe4a39fd0d5c
Mike Goldin <mike.goldin@consensys.net>
2018-01-18 16:35:30 -0500

    Add support for token allocations on test networks

--- migrations/2_optional_for_test.js
+++ migrations/2_optional_for_test.js
@@ -2,18 +2,37 @@
 
 const Token = artifacts.require('tokens/eip20/EIP20.sol');
 
+const BN = require('bignumber.js');
+const fs = require('fs');
+
 module.exports = (deployer, network, accounts) => {
+  const totalSupply = new BN('1000000000000000000000000', 10);
+  const decimals = '18';
+
   async function giveTokensTo(addresses) {
     const token = await Token.deployed();
     const user = addresses[0];
+    const allocation = totalSupply.div(new BN(accounts.length, 10));
+
+    console.log('Allocating ' +
+    `${allocation.toString(10).slice(0, allocation.toString(10).length - 18)}` +
+    ' TEST tokens to: ', user);
     await token.transfer(user, '100000');
-    if (addresses.length === 1) { return true; }
-    return giveTokensTo(addresses.slice(1));
+
+    if (addresses.length === 1) { return; }
+    giveTokensTo(addresses.slice(1));
   }
 
-  if (network === 'test') {
-    deployer.deploy(Token, '1000000', 'TestCoin', '0', 'TEST')
-      .then(() => giveTokensTo(accounts));
+  const config = JSON.parse(fs.readFileSync('./conf/config.json'));
+
+  if (network !== 'mainnet') {
+    deployer.deploy(Token, totalSupply, 'TestCoin', decimals, 'TEST')
+      .then(() => {
+        if (network === 'test') {
+          return giveTokensTo(accounts);
+        }
+        return giveTokensTo(config.testnets[network].tokenHolders);
+      });
     return;
   }
   console.log('skipping optional deploy of test-only contracts.');
