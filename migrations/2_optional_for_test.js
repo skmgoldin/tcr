c959caa3644e5612e9fd274385d4a198721ba90f
Mike Goldin <mike.goldin@consensys.net>
2018-01-18 20:15:03 -0500

    Allocate tokens equally between specified accounts

--- migrations/2_optional_for_test.js
+++ migrations/2_optional_for_test.js
@@ -8,22 +8,26 @@ const fs = require('fs');
 module.exports = (deployer, network, accounts) => {
   const totalSupply = new BN('1000000000000000000000000', 10);
   const decimals = '18';
+  const config = JSON.parse(fs.readFileSync('./conf/config.json'));
 
   async function giveTokensTo(addresses) {
     const token = await Token.deployed();
     const user = addresses[0];
-    const allocation = totalSupply.div(new BN(accounts.length, 10));
+    let allocation;
+    if (network === 'test') {
+      allocation = totalSupply.div(new BN(accounts.length, 10));
+    } else {
+      allocation = totalSupply.div(new BN(config.testnets[network].tokenHolders.length, 10));
+    }
 
     console.log('Allocating ' +
     `${allocation.toString(10).slice(0, allocation.toString(10).length - 18)}` +
     ' TEST tokens to: ', user);
-    await token.transfer(user, '100000');
+    await token.transfer(user, allocation);
 
     if (addresses.length === 1) { return true; }
     return giveTokensTo(addresses.slice(1));
   }
-
-  const config = JSON.parse(fs.readFileSync('./conf/config.json'));
 
   if (network !== 'mainnet') {
     deployer.deploy(Token, totalSupply, 'TestCoin', decimals, 'TEST')
