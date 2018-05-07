34262083b9abce25676b74b18eacb3fc26ebe37b
Mike Calvanese <mfcalvanese@gmail.com>
2018-05-07 12:46:53 -0500

    remove functions from ChallengeInterface
    the registry only needs to know if the challenge has ended, passed, and what the locked token amount is

--- contracts/Challenge/ChallengeInterface.sol
+++ contracts/Challenge/ChallengeInterface.sol
@@ -1,10 +1,7 @@
 pragma solidity ^0.4.8;
 
 contract ChallengeInterface {
-  function start() public;
-  function started() public view returns (bool);
   function ended() public view returns (bool);
   function passed() public view returns (bool);
   function tokenLockAmount() public view returns (uint);
-  function tokenRewardAmount() public view returns (uint);
 }
