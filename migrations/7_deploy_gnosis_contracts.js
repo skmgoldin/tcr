dcf524c67b7f466bdfdcbb632457a7245392745b
Mike Calvanese <mfcalvanese@gmail.com>
2018-05-15 01:18:42 -0400

    start adding market simulation script

--- migrations/7_deploy_gnosis_contracts.js
+++ migrations/7_deploy_gnosis_contracts.js
@@ -1,12 +1,14 @@
+const StandardMarketFactory = artifacts.require('StandardMarketFactory')
 const StandardMarketWithPriceLoggerFactory = artifacts.require('StandardMarketWithPriceLoggerFactory')
 const FutarchyChallengeFactory = artifacts.require('FutarchyChallengeFactory')
 const EventFactory = artifacts.require('EventFactory')
 const LMSRMarketMaker = artifacts.require('LMSRMarketMaker')
+const EtherToken = artifacts.require('EtherToken')
 const Math = artifacts.require('Math')
 
 module.exports = function (deployer) {
   deployer.deploy(
     Math
   )
-  deployer.link(Math, [StandardMarketWithPriceLoggerFactory, FutarchyChallengeFactory, EventFactory, LMSRMarketMaker])
+  deployer.link(Math, [EtherToken, StandardMarketFactory, StandardMarketWithPriceLoggerFactory, FutarchyChallengeFactory, EventFactory, LMSRMarketMaker])
 }
