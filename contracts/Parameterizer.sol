79563298456fd0d4e68d4068787fd9dfdf25bb5a
Isaac Kang <isaac.kang@consensys.net>
2018-05-16 08:43:18 -0400

    feat: parameterizer.claimRewards
    having to claimReward for multiple polls was a pain in the ass - it
    required sending multiple transactions, increasing the amount of gas
    fees, stress, and ultimately time it took to complete claiming more
    than one reward in an evening. this commit adds a function that resembles
    claimReward -- however instead of taking uints, it takes arrays of uints
    
    * add multiple claimRewards functionality
    * add tests

--- contracts/Parameterizer.sol
+++ contracts/Parameterizer.sol
@@ -262,6 +262,22 @@ contract Parameterizer {
     require(token.transfer(msg.sender, reward));
   }
 
+  /**
+  @dev                 Called by a voter to claim their rewards for each completed vote. Someone
+                        must call updateStatus() before this can be called.
+  @param _challengeIDs The PLCR pollIDs of the challenges rewards are being claimed for
+  @param _salts        The salts of a voter's commit hashes in the given polls
+  */
+  function claimRewards(uint[] _challengeIDs, uint[] _salts) public {
+      // make sure the array lengths are the same
+      require(_challengeIDs.length == _salts.length);
+
+      // loop through arrays, claiming each individual vote reward
+      for (uint i = 0; i < _challengeIDs.length; i++) {
+          claimReward(_challengeIDs[i], _salts[i]);
+      }
+  }
+
   // --------
   // GETTERS
   // --------
