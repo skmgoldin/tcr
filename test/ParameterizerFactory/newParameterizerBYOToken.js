7e74b5d514a66da6edef3244cd6898cef45bf4f3
Isaac Kang <isaac.kang@consensys.net>
2018-06-08 13:43:19 -0400

    ref: tests
    * rm: dependency of PLCRFactory deployment
    * adjust tests to support refactored semantics

--- test/ParameterizerFactory/newParameterizerBYOToken.js
+++ test/ParameterizerFactory/newParameterizerBYOToken.js
@@ -2,8 +2,6 @@
 /* global contract assert artifacts */
 
 const EIP20 = artifacts.require('tokens/eip20/EIP20.sol');
-const PLCRFactory = artifacts.require('./PLCRFactory.sol');
-const PLCRVoting = artifacts.require('./PLCRVoting.sol');
 const ParameterizerFactory = artifacts.require('./ParameterizerFactory.sol');
 const Parameterizer = artifacts.require('./Parameterizer.sol');
 const fs = require('fs');
@@ -12,12 +10,10 @@ const config = JSON.parse(fs.readFileSync('./conf/config.json'));
 const paramConfig = config.paramDefaults;
 
 contract('ParameterizerFactory', (accounts) => {
-  describe('Function: newParameterizerBYOTokenAndPLCR', () => {
+  describe('Function: newParameterizerBYOToken', () => {
     let parameterizerFactory;
-    let plcrFactory;
 
     before(async () => {
-      plcrFactory = await PLCRFactory.deployed();
       parameterizerFactory = await ParameterizerFactory.deployed();
     });
 
@@ -34,16 +30,6 @@ contract('ParameterizerFactory', (accounts) => {
         tokenParams.name,
         tokenParams.decimals,
         tokenParams.symbol,
-      );
-      // new plcr using factory/proxy
-      const plcrReceipt = await plcrFactory.newPLCRBYOToken(token.address);
-      const plcr = PLCRVoting.at(plcrReceipt.logs[0].args.plcr);
-      // verify: plcr's token is the same as the one we just deployed
-      const plcrToken = await plcr.token.call();
-      assert.strictEqual(
-        plcrToken,
-        token.address,
-        'the token connected to plcr is incorrect',
       );
 
       // new parameterizer using factory/proxy
@@ -62,7 +48,7 @@ contract('ParameterizerFactory', (accounts) => {
         paramConfig.pVoteQuorum,
       ];
       const parameterizerReceipt = await parameterizerFactory
-        .newParameterizerBYOTokenAndPLCR(token.address, plcr.address, parameters);
+        .newParameterizerBYOToken(token.address, parameters);
       const parameterizer = Parameterizer.at(parameterizerReceipt.logs[0].args.parameterizer);
       const { creator } = parameterizerReceipt.logs[0].args;
 
@@ -72,13 +58,6 @@ contract('ParameterizerFactory', (accounts) => {
         parameterizerToken,
         token.address,
         'the token connected to parameterizer is incorrect',
-      );
-      // verify: parameterizer's plcr
-      const parameterizerPLCR = await parameterizer.voting.call();
-      assert.strictEqual(
-        parameterizerPLCR,
-        plcr.address,
-        'the parameterizer\'s plcr is incorrect',
       );
       // verify: parameterizer's creator
       assert.strictEqual(creator, accounts[0], 'the creator emitted in the newParameterizer event ' +
