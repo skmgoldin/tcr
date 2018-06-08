ac124acffba0acaba3c72e439312edb1a66826a8
Isaac Kang <isaac.kang@consensys.net>
2018-06-08 13:38:16 -0400

    ref: migrations
    * move libs migrations to its own file
    * add condition to deploy non-mainnet PLCRFactory
    * add condition to reference the correct PLCRFactory address in
      parameterizer_factory migration

--- migrations/5_registry_factory.js
+++ migrations/5_registry_factory.js
@@ -3,11 +3,12 @@
 const RegistryFactory = artifacts.require('./RegistryFactory.sol');
 const DLL = artifacts.require('dll/DLL.sol');
 const AttributeStore = artifacts.require('attrstore/AttributeStore.sol');
+const ParameterizerFactory = artifacts.require('./ParameterizerFactory.sol');
 
 module.exports = (deployer) => {
   // link libraries
   deployer.link(DLL, RegistryFactory);
   deployer.link(AttributeStore, RegistryFactory);
 
-  deployer.deploy(RegistryFactory);
+  return deployer.deploy(RegistryFactory, ParameterizerFactory.address);
 };
