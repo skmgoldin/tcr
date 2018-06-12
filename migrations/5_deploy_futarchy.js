07d818adf8e20f11ae40630a9246631d60f632db
Mike Calvanese <mfcalvanese@gmail.com>
2018-06-12 19:05:57 -0400

    fix rebasing conflicts

--- migrations/5_deploy_futarchy.js
+++ migrations/5_deploy_futarchy.js
@@ -6,16 +6,18 @@ const StandardMarketFactory = artifacts.require('StandardMarketFactory')
 const StandardMarketWithPriceLoggerFactory = artifacts.require('StandardMarketWithPriceLoggerFactory')
 const FutarchyChallengeFactory = artifacts.require('FutarchyChallengeFactory')
 const FutarchyOracleFactory = artifacts.require('FutarchyOracleFactory')
-const CentralizedOracleFactory = artifacts.require('CentralizedOracleFactory')
+const CentralizedTimedOracleFactory = artifacts.require('CentralizedTimedOracleFactory')
 const EventFactory = artifacts.require('EventFactory')
 const LMSRMarketMaker = artifacts.require('LMSRMarketMaker')
 const EtherToken = artifacts.require('EtherToken')
+const DutchExchange = artifacts.require('DutchExchangeMock')
 
 const fs = require('fs')
 const config = JSON.parse(fs.readFileSync('../conf/config.json'))
 const paramConfig = config.paramDefaults
 
 const tradingPeriod = 60 * 60
+const timeToPriceResolution = 60 * 60 * 24 * 7 // a week
 const futarchyFundingAmount = paramConfig.minDeposit * 10 ** 18
 
 module.exports = (deployer) => {
@@ -26,9 +28,10 @@ module.exports = (deployer) => {
 
     await deployer.deploy(EventFactory)
     await deployer.deploy(StandardMarketWithPriceLoggerFactory)
-    await deployer.deploy(CentralizedOracleFactory)
+    await deployer.deploy(CentralizedTimedOracleFactory)
     await deployer.deploy(StandardMarketFactory)
     await deployer.deploy(LMSRMarketMaker)
+    await deployer.deploy(DutchExchange)
     await deployer.deploy(
       FutarchyOracleFactory,
       EventFactory.address,
@@ -37,11 +40,14 @@ module.exports = (deployer) => {
     await deployer.deploy(
       FutarchyChallengeFactory,
       Token.address,
+      EtherToken.address,
       futarchyFundingAmount,
       tradingPeriod,
+      timeToPriceResolution,
       FutarchyOracleFactory.address,
-      CentralizedOracleFactory.address,
-      LMSRMarketMaker.address
+      CentralizedTimedOracleFactory.address,
+      LMSRMarketMaker.address,
+      DutchExchange.address
     )
   }).catch((err) => { throw err })
 };
